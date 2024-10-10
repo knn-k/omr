98b0781c61d5d84ad00f8a854ea1fd88e2a6967e
KONNO Kazuhiro <konno@jp.ibm.com>
2024-10-10 11:53:54 +0900

    AArch64: Implement arrayTranslateTRTO
    This commit implements arrayTranslateTRTO for AArch64.
    
    Signed-off-by: KONNO Kazuhiro <konno@jp.ibm.com>

--- compiler/aarch64/codegen/OMRCodeGenerator.cpp
+++ compiler/aarch64/codegen/OMRCodeGenerator.cpp
@@ -190,10 +190,23 @@ OMR::ARM64::CodeGenerator::initialize()
          cg->setSupportsArrayCmpLen();
          }
       }
-      if (!comp->getOption(TR_DisableArraySetOpts))
-         {
-         cg->setSupportsArraySet();
-         }
+
+   if (!comp->getOption(TR_DisableArraySetOpts))
+      {
+      cg->setSupportsArraySet();
+      }
+
+   static bool disableTRTO = (feGetEnv("TR_disableTRTO") != NULL);
+   if (!disableTRTO)
+      {
+      cg->setSupportsArrayTranslateTRTO();
+      }
+
+   static bool disableTRTO255 = (feGetEnv("TR_disableTRTO255") != NULL);
+   if (!disableTRTO255)
+      {
+      cg->setSupportsArrayTranslateTRTO255();
+      }
    }
 
 void
